d1360fd96ae2643804c75b2d1f916d40a18a402d
JohnDoe <166081472+GamerRyker@users.noreply.github.com>
2026-09-03 20:24:48 -0600

    Update Chrome policies in PollenFS.sh
    Updated the PollenFS.sh script to modify Chrome policies, including changes to the ExtensionInstallForcelist and added ManagedBookmarks.

--- PollenFS.sh
+++ PollenFS.sh
@@ -19,7 +19,10 @@ echo "May Ultrablue Rest in Peace, o7"
 sleep 1
 
 mkdir -p /etc/opt/chrome/policies/managed
-echo '{"SecondaryGoogleAccountSigninAllowed": true,
+
+cat << 'EOF' > /etc/opt/chrome/policies/managed/pollen.json
+{
+  "SecondaryGoogleAccountSigninAllowed": true,
   "URLBlocklist": [],
   "EditBookmarksEnabled": true,
   "ChromeOsMultiProfileUserBehavior": "unrestricted",
@@ -34,7 +37,7 @@ echo '{"SecondaryGoogleAccountSigninAllowed": true,
   "ExtensionAllowedTypes": null,
   "ExtensionInstallAllowlist": null,
   "ExtensionInstallBlocklist": null,
-  "ExtensionInstallForcelist": [$EXTENSION_LIST],
+  "ExtensionInstallForcelist": [],
   "ExtensionSettings": null,
   "ChromeOsLockOnIdleSuspend": false,
   "PasswordManagerEnabled": true,
@@ -47,28 +50,23 @@ echo '{"SecondaryGoogleAccountSigninAllowed": true,
   "ForceYouTubeRestrict": 0,
   "EasyUnlockAllowed": true,
   "DisableSafeBrowsingProceedAnyway": false,
-  "DeviceAllowNewUsers": true,
-  "DevicePowerAdaptiveChargingEnabled": true,
-  "DeviceGuestModeEnabled": true,
-  "BrowserGuestModeEnabled": true,
-  "DeviceUnaffiliatedCrostiniAllowed": true,
+
   "VirtualMachinesAllowed": true,
   "CrostiniAllowed": true,
-  "DefaultCookiesSetting": 1,
   "VmManagementCliAllowed": true,
+  "UserBorealisAllowed": true,
+
+  "DefaultCookiesSetting": 1,
   "WifiSyncAndroidAllowed": true,
   "DeveloperToolsDisabled": false,
-  "DeviceBlockDevmode": false,
-  "UserBorealisAllowed": true,
-  "InstantTetheringAllowed": true,
   "NearbyShareAllowed": true,
-  "PinnedLauncherApps": null,
+  "InstantTetheringAllowed": true,
   "PrintingEnabled": true,
-  "DeviceReportNetworkEvents": false,
   "SmartLockSigninAllowed": true,
   "PhoneHubAllowed": true,
   "LacrosAvailability": "user_choice",
   "WallpaperImage": null,
+
   "ArcEnabled": true,
   "ArcPolicy": {
     "applications": [],
@@ -80,11 +78,10 @@ echo '{"SecondaryGoogleAccountSigninAllowed": true,
       "softwareInfoEnabled": true
     }
   },
-  
+
   "DeviceAutoUpdateTimeRestrictions": [],
   "DnsOverHttpsMode": "automatic",
   "BrowserLabsEnabled": true,
-  "ChromeOsReleaseChannelDelegated": true,
   "SafeSitesFilterBehavior": 0,
   "SafeBrowsingProtectionLevel": 0,
   "DownloadRestrictions": 0,
@@ -93,10 +90,37 @@ echo '{"SecondaryGoogleAccountSigninAllowed": true,
   "NetworkThrottlingEnabled": false,
   "NetworkPredictionOptions": 0,
   "AllowedDomainsForApps": "",
-  "DeviceUserAllowlist": "",
-  "DeviceAllowNewUser": 3,
   "SystemFeaturesDisableList": [],
-}' > /etc/opt/chrome/policies/managed/pollen.json
+
+  "ManagedBookmarks": [
+    { "toplevel_name": "Staff Bookmarks" },
+
+    { "name": "Adobe Creative Cloud", "url": "http://creativecloud.adobe.com/" },
+    { "name": "Clubs Directory", "url": "https://docs.google.com/spreadsheets/d/1yxpG3s5IpSAiXJEAiQzeQhd8TOj-N6Y6pI4E9GwxTqA/edit?gid=0#gid=0" },
+    { "name": "Explore Academy website", "url": "https://explore.academy/abq" },
+    { "name": "Google Classroom", "url": "https://classroom.google.com" },
+    { "name": "Google Play Books", "url": "https://play.google.com/books" },
+    { "name": "Hall Pass Request", "url": "https://hallpass.goguardian.com/home" },
+    { "name": "PowerSchool", "url": "https://exploreacademy.powerschool.com/guardian/home.html?_userTypeHint=student" },
+    { "name": "Screen Recorder", "url": "https://toolbox.googleapps.com/apps/screen_recorder/" },
+    { "name": "StopIt", "url": "https://web.stopitsolutions.com/login/" },
+    { "name": "Student Council Suggestions", "url": "https://docs.google.com/forms/d/1_cfUrJTXnLMAIzd3qc_Bx3NT7o2Hxd2gznVzl53qJUs/viewform?edit_requested=true" },
+    { "name": "Tech Support", "url": "https://docs.google.com/forms/d/e/1FAIpQLSdej7X29EGgJ3f7QNq5RJ7VNpmPHiYWesF-p4fW-6CVAxNWnA/viewform" },
+    { "name": "TI-84 Graphing Calculator Emulator", "url": "https://docs.google.com/document/d/1Ot1vD_jx12ccGAfnS1lNaYeIUUlv1abojGiIVU3-EP8/edit?usp=sharing" },
+    { "name": "Pear Assessment", "url": "https://assessment.peardeck.com/login" },
+
+    { "name": "Staff Dashboard", "url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vQRpwbH6MNfmQw_dRK1Adn8Iw8jE6gFE0MdGIOx-fD0nFVSC-iwzLPrDwr5qpqsui1bVcOX_V8tjIYB/pubhtml?gid=0&single=true" },
+    { "name": "Discipline Referral Form", "url": "https://docs.google.com/forms/d/e/1FAIpQLSeA7r4dZyVDp6mElxOPd9uPwR5pDTIqsd5GgYE4Mwk-idBEsQ/viewform" },
+    { "name": "Staff Directory", "url": "https://docs.google.com/document/d/1H6JasuBmIukLIhT7i8cs63lRrUXQ9zSH-LDvnAG2bno/edit?usp=sharing" },
+    { "name": "Staff Handbook", "url": "https://docs.google.com/document/d/1QRJx3F48m9t9Sbm6sL9ZNQCRf2AARe5oHRKA1YsXVKw/edit?usp=sharing" },
+    { "name": "Missing Assignment Report", "url": "https://docs.google.com/forms/d/e/1FAIpQLSfAt29Glo0aNzgAjNlaMdTHVD5wd3j44uD3oDy785Q6yCXE-g/viewform" },
+    { "name": "Pre-Referral Interventions", "url": "https://docs.google.com/document/d/1vh0x6FhqgaZ8htR1PU1PNpia6-Hx-4zO/edit?usp=sharing" },
+    { "name": "GoGuardian Teacher", "url": "https://teacher.goguardian.com/classrooms/active" },
+    { "name": "26-27 Staff Leave Request Form", "url": "https://docs.google.com/forms/d/1GCsj4YOLXx-eNQXdemsoArwLUVqjeWSZWc61wePvr3U/viewform" },
+    { "name": "2026-2027 Staff Calendar", "url": "https://docs.google.com/spreadsheets/d/18xtNopP5fnUNZLRPaay2gFN69CvxD6GWG57ZovQ6r9s/edit?usp=sharing" }
+  ]
+}
+EOF
 
 echo ""
 echo "Pollen (RootFS) has been successfully applied!"
